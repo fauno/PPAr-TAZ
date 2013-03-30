2fa1bbcfd9cd62e78b3cf0245181103b6ccb9185
Nicolás Reynolds <fauno@kiwwwi.com.ar>
2013-03-30 16:19:48 -0300

    Usar {SSHA}

--- passwd/helpers/taz_ldap_passwd.rb
+++ passwd/helpers/taz_ldap_passwd.rb
@@ -1,26 +1,35 @@
 require 'ldap'
+require 'digest'
+require 'base64'
 
 Passwd.helpers do
   # Basada en
   # http://www.iflowfor8hours.info/2012/02/ruby-webbased-ldap-password-changer/
   def taz_ldap_passwd(username, current, new)
-    LDAP::Conn.set_option(LDAP::LDAP_OPT_PROTOCOL_VERSION, 3)
 
     conn = LDAP::Conn.new('127.0.0.1', LDAP::LDAP_PORT)
+    conn.set_option(LDAP::LDAP_OPT_PROTOCOL_VERSION, 3)
 
+    # FIXME atrapar LDAP::ResultError (Invalid credentials)
     conn.bind("mail=#{username}@partidopirata.com.ar,ou=pirates,dc=PPAR.local", "#{current}")
 
-    changepw=[LDAP::Mod.new(LDAP::LDAP_MOD_REPLACE, 'userPassword', ["#{new}"]),]
+    changepw=[LDAP::Mod.new(LDAP::LDAP_MOD_REPLACE, 'userPassword', [taz_ssha(new)]),]
 
-    # FIXME por qué no agarra LDAP::ResultError (Invalid credentials)
     begin
       conn.modify("mail=#{username}@partidopirata.com.ar,ou=pirates,dc=PPAR.local", changepw)
       @return = true
-    rescue LDAP::ResultError
+    rescue LDAP::ResultError => reason
       @return = false
     end
 
     conn.unbind
     @return
+  end
+
+# http://youreference.blogspot.com.br/2010/10/web-based-ldap-reset-password.html
+  def taz_ssha(password)
+    salt = rand.hash.to_s
+
+    "{SSHA}"+Base64.encode64(Digest::SHA1.digest("#{password}#{salt}")+salt).chomp!
   end
 end
